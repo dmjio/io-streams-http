f9438e8fc3fe593a61e7493b0449839f91ac24bd
David Johnson <djohnson.m@gmail.com>
2014-12-19 02:29:53 -0600

    updated docs + examples, bumped version

--- src/System/IO/Streams/HTTP.hs
+++ src/System/IO/Streams/HTTP.hs
@@ -23,18 +23,21 @@
 -- > module Main where
 -- > import           System.IO.Streams       ( InputStream, OutputStream )
 -- > import qualified System.IO.Streams as    Streams
--- > import           System.IO.Streams.HTTP  ( withHTTP, parseUrl, withManager )
+-- > import           System.IO.Streams.HTTP  ( withHTTP, parseUrl, withManager, stream )
 -- > import           Network.HTTP.Client.TLS ( tlsManagerSettings )
 -- > import           Network.HTTP.Client     ( responseBody )
--- > 
--- > main :: IO ()
--- > main = do
--- >  req <- parseUrl "http://www.worldslongestwebsite.com"
--- >  withManager tlsManagerSettings $ \m ->
--- >    withHTTP req m $ \resp -> do
--- >      Streams.supplyTo Streams.stdout (responseBody resp)
 -- >
---
+-- > bodyTest :: IO ()
+-- > bodyTest = do
+-- >   req <- parseUrl "https://google.com"
+-- >   let request = req { method = "POST"
+-- >                     , requestBody = stream $ Streams.fromLazyByteString "body"
+-- >                     }
+-- >   withManager tlsManagerSettings $ \m ->
+-- >     withHTTP req m $ \resp -> do
+-- >         Streams.supplyTo Streams.stdout (responseBody resp)   
+-- >
+-- >
 -- For non-streaming request bodies, study the 'RequestBody' type,
 -- which also
 -- accepts strict \/ lazy bytestrings
